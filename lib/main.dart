d46b9ac829f3551d0e67993579dcc8f5f9d5d717
Bartlomiej Rasztabiga <contact@rasztabiga.me>
2020-10-27 23:06:47 +0100

    Add ReorderableListView

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,9 @@ class TodaysSchedule extends StatefulWidget {
 
 class _TodaysScheduleState extends State<TodaysSchedule> {
   final _entries = <ScheduleEntry>[
-    new ScheduleEntry("9:00", "10:00", "test"),
-    new ScheduleEntry("10:00", "11:00", "test"),
-    new ScheduleEntry("10:00", "11:00", "test")
+    new ScheduleEntry(1, "9:00", "10:00", "test"),
+    new ScheduleEntry(2, "10:00", "11:00", "test"),
+    new ScheduleEntry(3, "10:00", "11:00", "test1")
   ];
   final _biggerFont = TextStyle(fontSize: 18.0);
 
@@ -37,33 +37,54 @@ class _TodaysScheduleState extends State<TodaysSchedule> {
   }
 
   Widget _buildSchedule() {
-    return ListView.separated(
-        padding: EdgeInsets.all(4.0),
-        itemCount: _entries.length,
-        itemBuilder: (context, i) {
-          return _buildRow(_entries[i]);
-        },
-        separatorBuilder: (context, i) {
-          return Divider();
+    return ReorderableListView(
+        children: _entries.map((item) => _buildRow(item)).toList(),
+        onReorder: (int start, int current) {
+          // dragging from top to bottom
+          if (start < current) {
+            int end = current - 1;
+            ScheduleEntry startItem = _entries[start];
+            int i = 0;
+            int local = start;
+            do {
+              _entries[local] = _entries[++local];
+              i++;
+            } while (i < end - start);
+            _entries[end] = startItem;
+          }
+          // dragging from bottom to top
+          else if (start > current) {
+            ScheduleEntry startItem = _entries[start];
+            for (int i = start; i > current; i--) {
+              _entries[i] = _entries[i - 1];
+            }
+            _entries[current] = startItem;
+          }
+          setState(() {});
         });
   }
 
-  Widget _buildRow(ScheduleEntry pair) {
+  Widget _buildRow(ScheduleEntry entry) {
     return ListTile(
+        key: Key(entry.index.toString()),
         title: Text(
-          pair.getHourHeader(),
+          entry.getHourHeader(),
           style: _biggerFont,
         ),
-        subtitle: Text(pair.getTaskTitle()));
+        subtitle: Text(entry.getTaskTitle()),
+        trailing: Icon(Icons.menu));
   }
 }
 
 class ScheduleEntry {
+  int index;
   String startingHour;
   String endingHour;
   String title;
 
-  ScheduleEntry(String startingHour, String endingHour, String title) {
+  ScheduleEntry(
+      int index, String startingHour, String endingHour, String title) {
+    this.index = index;
     this.startingHour = startingHour;
     this.endingHour = endingHour;
     this.title = title;
